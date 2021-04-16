322a4e80070081bfb191003823ffd827428c314b
Kuchibecka <46870793+Kuchibecka@users.noreply.github.com>
2021-04-16 23:58:46 +0300

    15.04.21 Cosmetics

--- src/App.js
+++ src/App.js
@@ -6,31 +6,38 @@ import Objects from "./component/Objects";
 import ObjectForm from "./component/ObjectForm";
 import FetchedObjects from "./component/FetchedObjects";
 
+// todo: Update node, create node and connection
+//  front:  https://reactflow.dev/examples/update-node/
+//  back:   repo.save() method
+
 
 export default function App() {
     return [
-        <div className="container pt-3">
+        <span className="boder">
+        <div className="row">
+            <h1 align="center">ASU TP Reactive app</h1>
+        </div>
+        </span>,
+        <div className="col-sm-12">
             <div className="row">
-                <div className="col">
-                    <h1>ASU TP Reactive app</h1>
-                </div>
-                <div className="col">
+                <div className="col-sm-2">
                     <Objects/>
                 </div>
-                <div className="col">
+                <div className="col-sm-8">
+                    <GraphComponent/>
+                </div>
+                <div className="col-sm-2">
                     <FetchedObjects/>
                 </div>
+                <div className="row">
+                    <div className="container">
+                        <ObjectForm/>
+                        <ObjectComponent/>
+                    </div>
+                </div>
             </div>
-            <div className="container pt-3">
-                <ObjectForm/>
-            </div>
-        </div>,
-        <div className="container pt-2">
-            <ObjectComponent/>
-        </div>,
-        <div className="container pt-xl-1">
-            <GraphComponent/>
-        </div>,
+        </div>
+
     ]
 }
 
