b7ec03eb2b8f216523950afc3186b73e789ae6f9
Kuchibecka <46870793+Kuchibecka@users.noreply.github.com>
2021-04-18 00:32:00 +0300

    17.04.21 Tried to create a save() method, but failed :(

--- src/component/ObjectForm.js
+++ src/component/ObjectForm.js
@@ -1,68 +1,116 @@
 import React from "react";
 import {connect} from 'react-redux'
-import {createObject, showAlert} from "../redux/actions";
+import {showAlert} from "../redux/actions";
 import {Alert} from "./Alert";
+import ObjectService from "../service/ObjectService";
 
 class ObjectForm extends React.Component {
 
     constructor(props) {
         super(props);
-
         this.state = {
+            type: '',
             name: ''
         }
+
+        this.changeInputHandler = this.changeInputHandler.bind(this)
     }
 
     submitHandler = event => {
         event.preventDefault()
+        console.log(event.target.type.value)
+        const type = event.target.type.value
+        const name = event.target.name.value
+        const newObject = {
+            obj_id: 16,
+            type: Number(type),
+            name: name,
+            virusList: [],
+            securitySWList: [],
+            objectList: [],
+            andCriteriaList: []
+        }
+        console.log("New object: ", newObject)
+        console.log("New object in JSON format: ", JSON.stringify(newObject))
 
-        const {name} = this.state
+        ObjectService.createObject(JSON.stringify(newObject))
+        this.state = {name: '', type: ''}
+        // console.log("Current state is ", this.state)
 
+
+        /*
+        const {type, name} = this.state
+        console.log(type, name)
+        */
+
+        /*
         if (name.trim() === '') {
             return this.props.showAlert('Название не может быть пустым!')
         }
-
-        const newObject = {
-            name, id: Date.now().toString()
+        if (type.trim() === '') {
+            return this.props.showAlert('Тип не может быть пустым!')
         }
-
-        console.log(newObject)
-        this.props.createObject(newObject)
-        this.setState({name: ''})
+        const newObject = {
+            type, name
+        }
+        console.log("newObject: ", newObject)
+        // todo: post response
+        // this.props.createObject(newObject)
+        // todo: post response
+        this.setState({name: '', type: ''})
+        */
     }
 
-    changeInputHandler = event => {
+    changeInputHandler(event) {
         event.persist()
-        this.setState(prev => ({...prev, ...{
-            [event.target.name]: event.target.value
-        }}))
+        const target = event.target;
+        // console.log(event)
+        const key = target.id
+        const value = target.value
+        // console.log("Key: ", key, " Fetched value: ", value)
+        this.setState({
+            [key]: value
+        });
+        // console.log(this.state)
     }
 
     render() {
-        return(
-        <form onSubmit={this.submitHandler}>
+        return (
+            <form method="post"
+                  enctype="application/x-www-form-urlencoded"
+                  action="http://localhost:8081/api/object/new" onSubmit={this.submitHandler}>
+                {this.props.alert && <Alert text={this.props.alert}/>}
 
-            {this.props.alert && <Alert text={this.props.alert} />}
-
-            <div className="form-group">
-                <label htmlFor="name" className="form-label">Object name</label>
-                <input
-                    type="text"
-                    className="form-control"
-                    id="name"
-                    value={this.state.name}
-                    name="name"
-                    onChange={this.changeInputHandler}
-                />
-            </div>
-            <button className="btn btn-success" type="submit">Создать</button>
-        </form>
+                <div className="form-group">
+                    <label htmlFor="type" className="form-label">Object type</label>
+                    <input
+                        type="number"
+                        className="form-control"
+                        id="type"
+                        value={this.state.type}
+                        name="type"
+                        onChange={this.changeInputHandler}
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="name" className="form-label">Object name</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="name"
+                        value={this.state.name}
+                        name="name"
+                        onChange={this.changeInputHandler}
+                    />
+                </div>
+                <button className="btn btn-success" type="submit">Создать</button>
+            </form>
         )
     }
 }
 
 const mapDispatchToProps = {
-    createObject, showAlert
+    /*createObject,*/ showAlert
 }
 
 const mapStateToolProps = state => ({
--- src/service/ObjectService.js
+++ src/service/ObjectService.js
@@ -6,6 +6,15 @@ class ObjectService {
         return fetch(OBJECT_REST_API_URL)
             .then((res => res.json()));
     }
+
+    createObject(data) {
+        fetch('http://localhost:8081/api/object/new', {
+            mode: 'no-cors',
+            method: 'POST',
+            body: data
+        })
+            .then(r => console.log(r))
+    }
 }
 
 export default new ObjectService();
